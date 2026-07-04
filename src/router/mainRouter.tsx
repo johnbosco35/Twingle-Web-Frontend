6a93bd8cc6f5bac82250df28a78d0e27106ab683
Ezeh-fx <emekaezeh2009@gmail.com>
2026-07-04 15:21:11 +0100

    ready

--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -2,11 +2,10 @@ import { lazy } from "react";
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 import Layout from "@/components/layout/layout";
 import { withSuspense } from "@/router/withSuspense";
-import LoginPage from "@/components/auth/userAuth/login";
-import SignupPage from "@/components/auth/userAuth/signup";
-import SellerSignupPage from "@/components/auth/sellerAuth/sellerSignup";
-import ForgotPasswordPage from "@/components/auth/userAuth/forgotPassword";
-import VerifyOtpPage from "@/components/auth/userAuth/verifyOtp";
+import LoginPage from "@/components/auth/user/login";
+import SignupPage from "@/components/auth/user/signup";
+import ForgotPasswordPage from "@/components/auth/user/forgotPassword";
+import VerifyOtpPage from "@/components/auth/user/verifyOtp";
 
 import Signupvendor from "@/components/auth/vendor/Signupvendor";
 import ForgotPasswordvendor from "@/components/auth/vendor/forgotPasswordvendor";
@@ -28,10 +27,6 @@ const routes: RouteObject[] = [
   {
     path: "/signup",
     element: <SignupPage />,
-  },
-  {
-    path: "/seller-signup",
-    element: <SellerSignupPage />,
   },
   {
     path: "/select-account",
